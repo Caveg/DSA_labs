ca00bceb7e5632d6c97033ab3bba23db15004b5f
Caveg <gacsdvd@gmail.com>
2025-05-06 16:00:31 +0300

    lab11,12

--- labs/lab_11/main.c
+++ labs/lab_11/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include "headers/binary_tree.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    Node *root = createNewNode(5);
+    insertLeft(root, 3);
+    insertRight(root, 1);
+    Node *temp = insertLeft(root->left, 4);
+    insertRight(root->left, 2);
+    //insertLeft(temp, 8);
+    //insertRight(temp, 9);
+    insertLeft(root->right, 6);
+    //insertRight(root->right, 7);
+
+    ///traversals
+    printf("\nPreorder: ");
+    preorderTraversal(root);
+    printf("\nInorder: ");
+    inorderTraversal(root);
+    printf("\nPostorder: ");
+    postorderTraversal(root);
+
+    ///deallocate
+    destroyBinaryTree(&root);
+
+
     return 0;
 }
--- labs/lab_12/main.c
+++ labs/lab_12/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#include "headers/binary_search_tree.h"
+
+int main() {
+    srand(time(NULL));
+    Node *root = NULL;
+    // int n = 10;
+    // int items[10];
+
+    // for (int i = 0; i < n; ++i) {
+    //     items[i] = 500 + rand() % (978 - 500);
+    // }
+    //
+    // Node *root = createNewNode(items[0]);
+    // for (int i = 1; i < n; ++i) {
+    //     insert(root, items[i]);
+    // }
+
+    for (int i = 0; i < 10; ++i) {
+        int value = 500 + rand() % (978 - 500);
+        printf("Inserting: %d\n", value);
+        if (root == NULL) {
+            root = createNewNode(value);
+        } else {
+            insert(root, value);
+        }
+    }
+
+    printf("\nInorder: ");
+    inorderTraversal(root);
+    //
+    // deleteNode(root, 17);
+    // printf("\nInorder: ");
+    // inorderTraversal(root);
+    //
+    // deleteNode(root, 15);
+    // printf("\nInorder: ");
+    // inorderTraversal(root);
+    //
+    // deleteNode(root, 7);
+    // printf("\nInorder: ");
+    // inorderTraversal(root);
+    //
+    // destroyBinaryTree(&root);
+
     return 0;
 }
